fc813d031d29ef733f2c95fefe1e9d68b649baf2
fransoufil <fran.sf@uol.com.br>
2023-05-20 10:34:23 -0300

    Final, p.e. 12/05/2023, 05_maio

--- organizar_pgn.py
+++ organizar_pgn.py
@@ -1,11 +1,28 @@
 import os
 import chess.pgn
+import re
 
 # Caminho do diretório onde estão os arquivos PGN
 path_pgn = r"C:\teste - origem"
 
 # Caminho do diretório onde você quer salvar as partidas organizadas
 path_destino = r"C:\teste - destino"
+
+# Mapeamento de números de mês para nomes
+month_names = {
+    '1': 'janeiro', '01': 'janeiro',
+    '2': 'fevereiro', '02': 'fevereiro',
+    '3': 'março', '03': 'março',
+    '4': 'abril', '04': 'abril',
+    '5': 'maio', '05': 'maio',
+    '6': 'junho', '06': 'junho',
+    '7': 'julho', '07': 'julho',
+    '8': 'agosto', '08': 'agosto',
+    '9': 'setembro', '09': 'setembro',
+    '10': 'outubro',
+    '11': 'novembro',
+    '12': 'dezembro'
+}
 
 for filename in os.listdir(path_pgn):
     if filename.endswith('.pgn'):
@@ -20,12 +37,19 @@ for filename in os.listdir(path_pgn):
 
             # Extraia a data do primeiro jogo no arquivo
             date = game.headers["Date"]
-            year, month, day = date.split("/")
+            # Use a função re.split para dividir a data
+            year, month, day = re.split('[./-]', date)
+
+            # Traduzir o número do mês para o nome do mês
+            month_name = month_names[month.zfill(2)]
+
+            # Concatenar o número do mês e o nome do mês
+            month_folder = f"{month.zfill(2)}_{month_name}"
 
             last_dir = f"{day}{month}{year[2:]}"
 
             # Cria o diretório se ele não existir
-            new_dir = os.path.join(path_destino, year, month, last_dir)
+            new_dir = os.path.join(path_destino, year, month_folder, last_dir)
             os.makedirs(new_dir, exist_ok=True)
 
             # Move o arquivo para o novo diretório
