cc506514d30a04e0a69a3380663b5bc75e31d5d0
fransoufil <fran.sf@uol.com.br>
2023-05-23 15:33:51 -0300

    Varrendo as subpastas e usando chardet

--- organizar_pgn.py
+++ organizar_pgn.py
@@ -1,6 +1,11 @@
 import os
 import chess.pgn
 import re
+import datetime
+import shutil
+import chardet
+
+# pip install chardet --user
 
 # Caminho do diretório onde estão os arquivos PGN
 path_pgn = r"C:\teste - origem"
@@ -28,24 +33,42 @@ month_names = {
 successes = 0
 failures = 0
 
-for filename in os.listdir(path_pgn):
-    if filename.endswith('.pgn'):
-        with open(os.path.join(path_pgn, filename)) as pgn_file:
-            game = chess.pgn.read_game(pgn_file)
 
+def process_file(filename, path_pgn, path_destino):
+    try:
+        # Detectar a codificação do arquivo
+        with open(os.path.join(path_pgn, filename), 'rb') as pgn_file:
+            raw_data = pgn_file.read()
+            encoding = chardet.detect(raw_data)['encoding']
+
+        # Abrir o arquivo com a codificação detectada
+        with open(os.path.join(path_pgn, filename), encoding=encoding) as pgn_file:
+            game = chess.pgn.read_game(pgn_file)
             # Fecha explicitamente o arquivo aqui
             pgn_file.close()
 
             if game is None:
-                continue
+                return False
 
-            # Extraia a data do primeiro jogo no arquivo
-            date = game.headers["Date"]
+            # Extrai a data do cabeçalho "Date" ou obtém a data de modificação do arquivo
+            if "Date" in game.headers:
+                date = game.headers["Date"]
+            else:
+                date = os.path.getmtime(os.path.join(path_pgn, filename))
+                # Converter o timestamp da data de modificação em uma string de data
+                date = datetime.datetime.fromtimestamp(date).strftime('%Y-%m-%d')
+
             # Use a função re.split para dividir a data
             year, month, day = re.split('[./-]', date)
 
             # Traduzir o número do mês para o nome do mês
-            month_name = month_names[month.zfill(2)]
+            if month.zfill(2) in month_names:
+                month_name = month_names[month.zfill(2)]
+            else:
+                # Se o número do mês for inválido, use o mês atual
+                now = datetime.datetime.now()
+                month = str(now.month).zfill(2)
+                month_name = month_names[month]
 
             # Concatenar o número do mês e o nome do mês
             month_folder = f"{month.zfill(2)}_{month_name}"
@@ -56,13 +79,27 @@ for filename in os.listdir(path_pgn):
             new_dir = os.path.join(path_destino, year, month_folder, last_dir)
             os.makedirs(new_dir, exist_ok=True)
 
-            try:
-                # Move o arquivo para o novo diretório
-                os.rename(os.path.join(path_pgn, filename), os.path.join(new_dir, filename))
-                successes += 1
-            except Exception as e:
-                print(f"Erro ao mover o arquivo {filename}: {e}")
-                failures += 1
+            # Move o arquivo para o novo diretório
+            shutil.move(os.path.join(path_pgn, filename), os.path.join(new_dir, filename))
+            return True
+    except Exception as e:
+        print(f"Erro ao mover o arquivo {filename}: {e}")
+        return False
+
+
+def process_directory(directory, path_pgn, path_destino):
+    global successes, failures
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith('.pgn'):
+                success = process_file(filename, root, path_destino)
+                if success:
+                    successes += 1
+                else:
+                    failures += 1
+
+
+process_directory(path_pgn, path_pgn, path_destino)
 
 print(f"Arquivos movidos com sucesso: {successes}")
 print(f"Arquivos não movidos: {failures}")
